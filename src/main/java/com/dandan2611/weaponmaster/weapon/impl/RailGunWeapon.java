036de69be7d747b42ea431cf2c968723cb267aa4
Adam CAVILLON <dandadounet@gmail.com>
2022-06-12 12:44:20 +0200

    Added railgun particles

--- src/main/java/com/dandan2611/weaponmaster/weapon/impl/RailGunWeapon.java
+++ src/main/java/com/dandan2611/weaponmaster/weapon/impl/RailGunWeapon.java
@@ -5,14 +5,13 @@ import com.dandan2611.weaponmaster.WeaponMaster;
 import com.dandan2611.weaponmaster.utils.ItemBuilder;
 import com.dandan2611.weaponmaster.weapon.InteractionListener;
 import com.dandan2611.weaponmaster.weapon.Weapon;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
+import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
 
 import java.util.HashMap;
 import java.util.UUID;
@@ -61,9 +60,29 @@ public class RailGunWeapon extends Weapon implements InteractionListener {
 
                         @Override
                         public void run() {
+                            // Sniping particles
                             Block targetBlock = player.getTargetBlock(null, Constants.MAX_RAILGUN_DISTANCE);
+                            Location playerLocation = player.getEyeLocation().subtract(0d, 0.5d, 0d);
+                            Vector playerDirection = playerLocation.getDirection();
+                            double distance = playerLocation.distance(targetBlock.getLocation());
+
+                            for(double d = 0d; d < distance; d += 0.1)  {
+                                Location particleLocation =
+                                        playerLocation.clone().add(playerDirection.clone().multiply(d));
+                                playerLocation.getWorld().spawnParticle(Particle.CRIT,
+                                        particleLocation,
+                                        1,
+                                        0d,
+                                        0d,
+                                        0d,
+                                        0d);
+                            }
+
+                            // Block breaking
                             if(!Material.AIR.equals(targetBlock.getType()))
                                 targetBlock.breakNaturally();
+
+                            // More iterations
                             iterations++;
                             if(iterations >= Constants.MAX_RAILGUN_TASK_ITERATIONS)
                                 Bukkit.getScheduler().cancelTask(playerTaskMap.get(uuid));
