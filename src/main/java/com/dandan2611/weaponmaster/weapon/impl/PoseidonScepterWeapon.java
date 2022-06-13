d940162e3839e994225f9a299c202af55750a181
Adam CAVILLON <dandadounet@gmail.com>
2022-06-13 10:59:44 +0200

    Added basic effects to poseidon scepter + Fixed move listener

--- src/main/java/com/dandan2611/weaponmaster/weapon/impl/PoseidonScepterWeapon.java
+++ src/main/java/com/dandan2611/weaponmaster/weapon/impl/PoseidonScepterWeapon.java
@@ -13,6 +13,7 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -34,33 +35,44 @@ public class PoseidonScepterWeapon extends Weapon implements InteractionListener
         super.setEventListener(this);
         this.activatedPlayers = new HashMap<>();
         this.scepterTaskId = Bukkit.getScheduler().scheduleSyncRepeatingTask(WeaponMaster.getInstance(),
-                () -> {
+                new Runnable() {
 
-                    double maxRadius = Constants.POSEIDON_SCEPTER_CYLINDER_MAX_RADIUS;
-                    int steps = Constants.POSEIDON_SCEPTER_CYLINDER_STEPS;
-                    int floors = Constants.POSEIDON_SCEPTER_NUM_FLOORS;
-                    double separation = Constants.POSEIDON_SCEPTER_FLOORS_SEPARATION;
+            int ticks = 0;
 
-                    for (Map.Entry<UUID, WeaponContext> entry : activatedPlayers.entrySet()) {
-                        WeaponContext context = entry.getValue();
-                        Location location = context.location.clone().add(0d, 0.25d, 0d);
+            @Override
+            public void run() {
+                double maxRadius = Constants.POSEIDON_SCEPTER_CYLINDER_MAX_RADIUS;
+                int steps = Constants.POSEIDON_SCEPTER_CYLINDER_STEPS;
+                int floors = Constants.POSEIDON_SCEPTER_NUM_FLOORS;
+                double separation = Constants.POSEIDON_SCEPTER_FLOORS_SEPARATION;
 
-                        double radius = context.radius + 0.25d;
-                        if(radius > maxRadius)
-                            radius = maxRadius;
-                        context.radius = radius;
+                for (Map.Entry<UUID, WeaponContext> entry : activatedPlayers.entrySet()) {
+                    WeaponContext context = entry.getValue();
+                    Location location = context.location.clone().add(0d, 0.25d, 0d);
 
-                        for (int i = 0; i < floors; i++) {
-                            for (double theta = 0; theta < 360; theta += 360d/steps) { // TODO: Constant
-                                double rad = Math.toRadians(theta);
-                                double x = Math.cos(rad) * radius;
-                                double z = Math.sin(rad) * radius;
-                                Location l = location.clone().add(x, 0, z);
-                                location.getWorld().spawnParticle(Particle.WATER_DROP, l, 1, 0d, 0d, 0d, 0d);
-                            }
-                            location.add(0d, separation, 0d);
+                    double radius = context.radius + 0.25d;
+                    if(radius > maxRadius)
+                        radius = maxRadius;
+                    context.radius = radius;
+
+                    for (int i = 0; i < floors; i++) {
+                        for (double theta = 0; theta < 360; theta += 360d/steps) { // TODO: Constant
+                            double rad = Math.toRadians(theta);
+                            double x = Math.cos(rad) * radius;
+                            double z = Math.sin(rad) * radius;
+                            Location l = location.clone().add(x, 0, z);
+                            location.getWorld().spawnParticle(Particle.WATER_DROP, l, 1, 0d, 0d, 0d, 0d);
                         }
+                        location.add(0d, separation, 0d);
                     }
+
+                    if(ticks % (3*20/Constants.POSEIDON_SCEPTER_TASK_TICKS) == 0) {
+                        location.getWorld().playSound(location, Sound.BLOCK_CONDUIT_AMBIENT, SoundCategory.PLAYERS, 1f, 1f);
+                    }
+                }
+
+                ticks++;
+            }
                 },
                 0L, Constants.POSEIDON_SCEPTER_TASK_TICKS);
 
@@ -95,6 +107,11 @@ public class PoseidonScepterWeapon extends Weapon implements InteractionListener
             }
             else { // Enable scepter
                 activatedPlayers.put(uuid, new WeaponContext(location, Constants.POSEIDON_SCEPTER_CYLINDER_START_RADIUS));
+
+                World world = location.getWorld();
+                if(world != null) {
+                    world.playSound(location, Sound.BLOCK_CONDUIT_ACTIVATE, SoundCategory.PLAYERS, 1f, 1f);
+                }
             }
 
             event.setCancelled(true);
@@ -107,9 +124,16 @@ public class PoseidonScepterWeapon extends Weapon implements InteractionListener
         UUID uuid = player.getUniqueId();
         WeaponContext context = activatedPlayers.get(uuid);
         if(context != null) {
-            Location location = context.location;
-            if (location.distance(player.getLocation()) > 0.25D)
-                player.teleport(location.clone().setDirection(player.getEyeLocation().getDirection()));
+            Location centerLocation = context.location;
+            if (centerLocation.distance(player.getLocation()) > Constants.POSEIDON_SCEPTER_CYLINDER_MAX_RADIUS) {
+                Vector direction = player.getEyeLocation().getDirection();
+                Location playerLocation = player.getLocation();
+                Vector playerToCenterVector = new Vector(playerLocation.getX() - centerLocation.getX(),
+                        playerLocation.getY() - centerLocation.getY(),
+                        playerLocation.getZ() - centerLocation.getZ());
+                playerToCenterVector.normalize();
+                player.teleport(event.getFrom().clone().setDirection(direction).subtract(playerToCenterVector.multiply(0.25d)));
+            }
         }
     }
 
