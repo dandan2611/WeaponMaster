01ae00ba92b2e86481a0c751021d13c0b99998d8
Adam CAVILLON <dandadounet@gmail.com>
2022-06-13 13:02:47 +0200

    Documented ItemUtils

--- src/main/java/com/dandan2611/weaponmaster/utils/ItemUtils.java
+++ src/main/java/com/dandan2611/weaponmaster/utils/ItemUtils.java
@@ -5,6 +5,12 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 public class ItemUtils {
 
+    /**
+     * Checks if two itemstacks are comparable (same type, same amount and same displayname, ignores meta)
+     * @param firstItem First item to compare
+     * @param secondItem Second item to compare
+     * @return If the two itemstacks matches the verified properties
+     */
     public static boolean isComparable(ItemStack firstItem, ItemStack secondItem) {
         ItemMeta firstItemMeta = firstItem.getItemMeta();
         ItemMeta secondItemMeta = secondItem.getItemMeta();
