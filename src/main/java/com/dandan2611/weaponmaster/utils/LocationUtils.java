aed9dc75fdd64b49694c97581908d5e50e2274fa
Adam CAVILLON <dandadounet@gmail.com>
2022-06-13 13:22:44 +0200

    Documented LocationUtils

--- src/main/java/com/dandan2611/weaponmaster/utils/LocationUtils.java
+++ src/main/java/com/dandan2611/weaponmaster/utils/LocationUtils.java
@@ -8,8 +8,17 @@ import org.bukkit.block.BlockFace;
 
 import java.util.Random;
 
+/**
+ * Location utils methods
+ */
 public class LocationUtils {
 
+    /**
+     * Determine a random location, with the same height, with given parameters
+     * @param location Center location
+     * @param xzRadius x and z random radius
+     * @return Random determined location
+     */
     public static Location randomLocation(Location location, int xzRadius) {
         Random random = new Random();
         int randX = random.nextInt(2 * xzRadius) - xzRadius;
@@ -17,6 +26,13 @@ public class LocationUtils {
         return new Location(location.getWorld(), location.getX() + randX, 0, location.getZ() + randZ);
     }
 
+    /**
+     * Determine a random spawnable location with given parameters
+     * @param location Center location
+     * @param xzRadius x and z random radius
+     * @param maxTries Maximum number of tries before returning the provided location
+     * @return Random determined location
+     */
     public static Location randomSpawnableLocation(Location location, int xzRadius, int maxTries) {
         Location l;
         int tries = 0;
@@ -33,6 +49,11 @@ public class LocationUtils {
         return l;
     }
 
+    /**
+     * Checks if a mob (2 blocks height) can be spawned at the specified location
+     * @param location Location to check
+     * @return If a mob can be spawned at the provided location
+     */
     public static boolean isSpawnable(Location location) {
         Block block = location.getBlock();
         Block topBlock = block.getRelative(BlockFace.UP);
