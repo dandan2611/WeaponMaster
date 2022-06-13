7d30034c3b6f3e280aa664b8925fd2fb23c6468f
Adam CAVILLON <dandadounet@gmail.com>
2022-06-13 12:13:42 +0200

    Created weapon cooldown methods

--- src/main/java/com/dandan2611/weaponmaster/weapon/Weapon.java
+++ src/main/java/com/dandan2611/weaponmaster/weapon/Weapon.java
@@ -1,16 +1,21 @@
 package com.dandan2611.weaponmaster.weapon;
 
 import com.dandan2611.weaponmaster.utils.ItemUtils;
-import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 public abstract class Weapon {
 
     protected InteractionListener interactionListener;
     protected Listener eventListener;
+    protected HashMap<UUID, Long> playerCooldownMap;
+    protected Long defaultCooldownTime;
 
     public Weapon(InteractionListener interactionListener) {
         this.interactionListener = interactionListener;
@@ -39,6 +44,19 @@ public abstract class Weapon {
             HandlerList.unregisterAll(eventListener);
     }
 
+    public void cooldown(OfflinePlayer player) {
+        playerCooldownMap.put(player.getUniqueId(), System.currentTimeMillis());
+    }
+
+    public boolean isInCooldown(OfflinePlayer player) {
+        return isInCooldown(player, defaultCooldownTime);
+    }
+
+    public boolean isInCooldown(OfflinePlayer player, Long cooldownTime) {
+        Long cooldownStartTime = playerCooldownMap.get(player.getUniqueId());
+        return cooldownStartTime != null && cooldownStartTime < System.currentTimeMillis() + cooldownTime;
+    }
+
     public InteractionListener getInteractionListener() {
         return interactionListener;
     }
@@ -53,6 +71,22 @@ public abstract class Weapon {
 
     public void setEventListener(Listener eventListener) {
         this.eventListener = eventListener;
+    }
+
+    public HashMap<UUID, Long> getPlayerCooldownMap() {
+        return playerCooldownMap;
+    }
+
+    public void setPlayerCooldownMap(HashMap<UUID, Long> playerCooldownMap) {
+        this.playerCooldownMap = playerCooldownMap;
+    }
+
+    public Long getDefaultCooldownTime() {
+        return defaultCooldownTime;
+    }
+
+    public void setDefaultCooldownTime(Long defaultCooldownTime) {
+        this.defaultCooldownTime = defaultCooldownTime;
     }
 
 }
